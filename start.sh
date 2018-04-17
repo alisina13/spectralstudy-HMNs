3da17ffb25e14364a1b37fcabe1ba3c63e196abf
Ali Safari <ali.s.safari@fau.de>
2018-04-17 13:14:50 +0200

    commit

--- start.sh
+++ start.sh
@@ -20,7 +20,7 @@
 EXEC="$HOME/Projects/hmn-percolation/percolation"
 
 INPUTFILE='FileReaderTestInput.txt'
-SIMPATH="$HOME/Projects/hmn-percolation/Results/laplacian"
+SIMPATH="$HOME/Projects/hmn-percolation/Results/laplacianTest"
 
 # run
 HOSTN=$(hostname)
